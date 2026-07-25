b76668ec16388613d42f41d701f929049d415322
jamisaikumar <jamisaikumar33@gmail.com>
2026-07-25 21:57:24 +0530

    Fixed indexjs

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,8 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(
@@ -32,6 +35,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
